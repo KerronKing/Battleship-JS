4564564fec41237d807830ec34b0fbd0f14d436a
evaveskova <evaveskova@gmail.com>
2020-01-31 16:42:04 +0100

    add initial getplayer function with event listener

---
+++ src/getplayer.js
@@ -0,0 +1,7 @@
+const getPlayer = (() => {
+  const inputForm = document.getElementById('user-form');
+  const playerInput = document.getElementById('form-input');
+  inputForm.addEventListener('submit', () => {
+    
+  })
+)}()
