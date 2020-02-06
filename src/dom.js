dfec2c73e7613ac7b56f2af6e8b70ec57c1df263
Eva Veskova Jackson <51458322+evaveskova@users.noreply.github.com>
2020-02-06 22:31:35 +0100

    rewrote ship tests

--- src/dom.js
+++ src/dom.js
@@ -1,28 +1,27 @@
 const dom = (() => {
   const playerAreaRender = () => {
-    const playerInterface = document.getElementById('player-area');
+    const playerInterface = document.getElementById("player-area");
     for (let i = 0; i < 100; i += 1) {
-      const area = document.createElement('DIV');
-      area.classList.add('square');
+      const area = document.createElement("DIV");
+      area.classList.add("square");
       area.id = `pa-${i}`;
       playerInterface.appendChild(area);
     }
   };
 
   const computerAreaRender = () => {
-    const computerInterface = document.getElementById('computer-area');
+    const computerInterface = document.getElementById("computer-area");
     for (let i = 0; i < 100; i += 1) {
-      const compArea = document.createElement('DIV');
-      compArea.classList.add('square');
+      const compArea = document.createElement("DIV");
+      compArea.classList.add("square");
       compArea.id = `ca-${i}`;
-      compArea.textContent = `${i}`;
       computerInterface.appendChild(compArea);
     }
   };
 
   const invalidMoveAlert = () => {
-    const status = document.getElementById('status');
-    status.textContent = 'Invalid move. Please play again.';
+    const status = document.getElementById("status");
+    status.textContent = "Invalid move. Please play again.";
   };
 
   return { playerAreaRender, computerAreaRender, invalidMoveAlert };
