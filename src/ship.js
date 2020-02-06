0ea6a762eec58089a90260bbcf8be24f49f1f4ef
KerronKing <king_of_palmiste@hotmail.com>
2020-02-06 11:46:32 -0400

    Add hit and isSunk functions to ship factory

--- src/ship.js
+++ src/ship.js
@@ -2,8 +2,19 @@ const Ship = (name, shipLength) => {
   const position = new Array(shipLength).fill('');
   const hitPositions = [];
 
+  const hit = (num) => {
+    this.hitPositions.push(num);
+  };
+
+  const isSunk = () => {
+    if (this.positions.length === this.hitPositions.length) {
+      return true;
+    }
+    return false;
+  };
+
   return {
-    name, shipLength, position, hitPositions,
+    name, shipLength, position, hitPositions, hit, isSunk,
   };
 };
 export default Ship;
