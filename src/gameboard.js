6db16bd21d31ab464a302413287d3de604ed94cc
Eva Veskova Jackson <51458322+evaveskova@users.noreply.github.com>
2020-02-06 21:51:52 +0100

    move gameWon function to gameboard and refactor start game, add additional styling to prevent overlaps

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,37 +1,37 @@
-import Ship from './ship';
-import mapper from './mapper';
+import Ship from "./ship";
+import mapper from "./mapper";
 
-const Gameboard = (name) => {
+const Gameboard = name => {
   const areaArray = [
-    ['', '', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', '', ''],
+    ["", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", ""]
   ];
   const ships = [];
 
-  const createShip = (length) => {
-    const directionArray = ['horizontal', 'vertical'];
+  const createShip = length => {
+    const directionArray = ["horizontal", "vertical"];
     const direction = directionArray[Math.floor(Math.random() * 2)];
 
     const num = Math.floor(Math.random() * 100);
     const posArray = [];
     for (let i = 0; i < length; i += 1) {
-      if (direction === 'vertical' && ((num + (length * 10)) <= 99)) {
-        posArray.push(num + (i * 10));
-      } else if (direction === 'vertical' && ((num + (length * 10)) >= 99)) {
-        const newNum = (num - 40);
-        posArray.push(newNum + (i * 10));
-      } else if (direction === 'horizontal' && ((num % 10) <= length)) {
+      if (direction === "vertical" && num + length * 10 <= 99) {
+        posArray.push(num + i * 10);
+      } else if (direction === "vertical" && num + length * 10 >= 99) {
+        const newNum = num - 40;
+        posArray.push(newNum + i * 10);
+      } else if (direction === "horizontal" && num % 10 <= length) {
         posArray.push(num + i);
-      } else if (direction === 'horizontal' && ((num % 10) >= length)) {
-        const newNum = (num - (length - 1));
+      } else if (direction === "horizontal" && num % 10 >= length) {
+        const newNum = num - (length - 1);
         posArray.push(newNum + i);
       }
     }
@@ -70,7 +70,7 @@ const Gameboard = (name) => {
 
       for (let j = 0; j < ship.length; j += 1) {
         const coords = mapper.numConverter(ship[j]);
-        areaArray[coords[0]][coords[1]] = 'ship';
+        areaArray[coords[0]][coords[1]] = "ship";
       }
       shipObjectsArray[i].position = ship;
     }
@@ -82,18 +82,38 @@ const Gameboard = (name) => {
     const position = mapper.coordConverter([x, y]);
 
     for (let i = 0; i < arr.length; i += 1) {
-      if (arr[i].position.indexOf(position) >= 0
-      && areaArray[x][y] === 'ship') {
+      if (
+        arr[i].position.indexOf(position) >= 0 &&
+        areaArray[x][y] === "ship"
+      ) {
         arr[i].hit(position);
-        areaArray[x][y] = 'hit';
-      } else if (areaArray[x][y] === '') {
-        areaArray[x][y] = 'missed';
+        areaArray[x][y] = "hit";
+      } else if (areaArray[x][y] === "") {
+        areaArray[x][y] = "missed";
       }
     }
   };
 
+  const gameWon = () => {
+    let counter = 0;
+    for (let j = 0; j < ships.length; j += 1) {
+      if (ships[j].isSunk()) {
+        counter += 1;
+      }
+    }
+    if (counter === 5) {
+      return true;
+    }
+    return false;
+  };
+
   return {
-    name, areaArray, ships, populateBoard, receiveAttack,
+    name,
+    areaArray,
+    ships,
+    populateBoard,
+    receiveAttack,
+    gameWon
   };
 };
 export default Gameboard;
--- src/gameflow.js
+++ src/gameflow.js
@@ -1,54 +1,42 @@
-import dom from './dom';
-import mapper from './mapper';
-import Gameboard from './gameboard';
-import Player from './player';
+import dom from "./dom";
+import mapper from "./mapper";
+import Gameboard from "./gameboard";
+import Player from "./player";
+import Ship from "./ship";
 
 const gameflow = (() => {
   let players = [];
   let boardObjects = [];
-  const status = document.getElementById('status');
-  const playerInterface = document.getElementById('player-area');
-  const computerInterface = document.getElementById('computer-area');
-  const grids = document.getElementById('grids');
+  const status = document.getElementById("status");
+  const playerInterface = document.getElementById("player-area");
+  const computerInterface = document.getElementById("computer-area");
+  const grids = document.getElementById("grids");
+  const restartBtn = document.getElementById("restart");
 
-  const generatePlayers = (data) => {
+  const generatePlayers = data => {
     const player = Player(data, 1);
-    const computer = Player('Computer', 0);
+    const computer = Player("Computer", 0);
     players.push(player);
     players.push(computer);
   };
 
-  const computerTarget = (arr) => {
+  const computerTarget = arr => {
     let index = Math.floor(Math.random() * 100);
     let coords = mapper.numConverter(index);
 
-    while (arr[coords[0]][coords[1]] === 'hit' || arr[coords[0]][coords[1]] === 'miss') {
+    while (
+      arr[coords[0]][coords[1]] === "hit" ||
+      arr[coords[0]][coords[1]] === "miss"
+    ) {
       index = Math.floor(Math.random() * 100);
       coords = mapper.numConverter(index);
     }
     return coords;
   };
 
-  const gameWon = (arr) => {
-    const boards = arr;
-    let counter;
-
-    for (let i = 0; i < boards.length; i += 1) {
-      for (let j = 0; j < boards[0].ships.length; j += 1) {
-        if (boards[i].ships[j].isSunk()) {
-          counter += 1;
-        }
-      }
-    }
-    if (counter === 5) {
-      return true;
-    }
-    return false;
-  };
-
   const runGame = () => {
-    const playerBoard = Gameboard('Player Board');
-    const computerBoard = Gameboard('Computer Board');
+    const playerBoard = Gameboard("Player Board");
+    const computerBoard = Gameboard("Computer Board");
     boardObjects.push(playerBoard);
     boardObjects.push(computerBoard);
 
@@ -62,9 +50,11 @@ const gameflow = (() => {
 
     Array.from(playerDivs).forEach((elem, j) => {
       const coords = mapper.numConverter(j);
-      if (boardObjects[0].areaArray[coords[0][coords[1]]] === 'ship'
-      || boardObjects[0].areaArray[coords[0][coords[1]]] === 'hit') {
-        elem.classList.add('player-ship');
+      if (
+        boardObjects[0].areaArray[coords[0][coords[1]]] === "ship" ||
+        boardObjects[0].areaArray[coords[0][coords[1]]] === "hit"
+      ) {
+        elem.classList.add("player-ship");
       }
     });
 
@@ -74,15 +64,17 @@ const gameflow = (() => {
         const num = mapper.coordConverter(crds);
         const target = document.getElementById(`pa-${num}`);
 
-        if (boardObjects[0].areaArray[crds[0]][crds[1]] === 'ship') {
-          target.classList.add('ship-hit');
+        if (boardObjects[0].areaArray[crds[0]][crds[1]] === "ship") {
+          status.textContent = "";
+          target.classList.add("ship-hit");
           boardObjects[0].receiveAttack(crds[0], crds[1]);
-          if (gameWon(boardObjects)) {
-            grids.classList.replace('visible', 'hidden');
+          if (boardObjects[0].gameWon()) {
+            grids.classList.replace("visible", "hidden");
             status.textContent = `${players[1].name} has won the game! Click restart to play again.`;
           }
-        } else if (boardObjects[0].areaArray[crds[0]][crds[1]] === '') {
-          target.classList.add('missed');
+        } else if (boardObjects[0].areaArray[crds[0]][crds[1]] === "") {
+          status.textContent = "";
+          target.classList.add("missed");
           boardObjects[0].receiveAttack(crds[0], crds[1]);
           players[0].moveNumber += 1;
           players[1].moveNumber += 1;
@@ -92,24 +84,26 @@ const gameflow = (() => {
     const computerDivs = computerInterface.children;
     Array.from(computerDivs).forEach((elem, i) => {
       const crds = mapper.numConverter(i);
-      elem.addEventListener('click', (e) => {
+      elem.addEventListener("click", e => {
         e.preventDefault();
         console.log(boardObjects[1].ships);
         console.log(boardObjects[1].ships[0].isSunk());
-        if (boardObjects[1].areaArray[crds[0]][crds[1]] === 'ship') {
-          elem.classList.add('ship-hit');
+        if (boardObjects[1].areaArray[crds[0]][crds[1]] === "ship") {
+          status.textContent = "";
+          elem.classList.add("ship-hit");
           boardObjects[1].receiveAttack(crds[0], crds[1]);
-          if (gameWon(boardObjects)) {
-            grids.classList.replace('visible', 'hidden');
+          if (boardObjects[1].gameWon()) {
+            grids.classList.replace("visible", "hidden");
             status.textContent = `${players[0].name} has won the game! Click restart to play again.`;
           }
-        } else if (boardObjects[1].areaArray[crds[0]][crds[1]] === '') {
-          elem.classList.add('missed');
+        } else if (boardObjects[1].areaArray[crds[0]][crds[1]] === "") {
+          status.textContent = "";
+          elem.classList.add("missed");
           boardObjects[1].receiveAttack(crds[0], crds[1]);
           players[0].moveNumber += 1;
           players[1].moveNumber += 1;
           computerMove();
-        } else if (boardObjects[1].areaArray[crds[0]][crds[1]] === 'hit') {
+        } else if (boardObjects[1].areaArray[crds[0]][crds[1]] === "hit") {
           dom.invalidMoveAlert();
         }
       });
@@ -118,26 +112,29 @@ const gameflow = (() => {
   const resetGame = () => {
     players = [];
     boardObjects = [];
-    playerInterface.innerHTML = '';
-    computerInterface.innerHTML = '';
+    playerInterface.innerHTML = "";
+    computerInterface.innerHTML = "";
+    const status = document.getElementById("status");
+    status.textContent = "";
+    restartBtn.classList.replace("visible", "hidden");
   };
   const start = () => {
-    const startBtn = document.getElementById('start');
-    const restartBtn = document.getElementById('restart');
-    const inputForm = document.forms['user-form'];
-    const formContainer = document.getElementById('input-container');
+    const startBtn = document.getElementById("start");
+    const restartBtn = document.getElementById("restart");
+    const inputForm = document.forms["user-form"];
+    const formContainer = document.getElementById("input-container");
 
-    inputForm.addEventListener('submit', (e) => {
+    inputForm.addEventListener("submit", e => {
       e.preventDefault();
       const data = Object.fromEntries(new FormData(inputForm));
       generatePlayers(data.name);
-      formContainer.classList.replace('visible', 'hidden');
-      startBtn.classList.replace('visible', 'hidden');
-      restartBtn.classList.replace('hidden', 'visible');
+      formContainer.classList.replace("visible", "hidden");
+      startBtn.classList.replace("visible", "hidden");
+      restartBtn.classList.replace("hidden", "visible");
       runGame();
     });
   };
 
-  return { start, resetGame, gameWon };
+  return { start, resetGame };
 })();
 export default gameflow;
